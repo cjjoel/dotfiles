f9a655766a5a9716a0f711845e685e37c64d3d29
Joel <cjjoelx42@gmail.com>
2021-01-31 15:00:14 +0530

    Add battery and date modules

--- configs/polybar.nix
+++ configs/polybar.nix
@@ -5,26 +5,32 @@
   config = {
     "bar/top" = {
       background="\${colors.background}";
-      border-color = "#00000000";
+      foreground = "\${colors.foreground}";
       border-size = 4;
       fixed-center = "false";
-      font-0 = "misc fixed:pixelsize=15";
-      foreground = "\${colors.foreground}";
-      height = 27;
       line-color = "#f00";
       line-size = 3;
+      
+      height = 27;
+      width = "100%";
+      radius = "6.0";
+      padding-left = 1;
+      padding-right = 1;
+
+      # font-0 = "misc fixed:pixelsize=15;2";
+      font-0 = "Monospace:style=Regular:size=14;2";
+      font-1 = "FontAwesome:size=14;2";
+      
       module-margin-left = 1;
       module-margin-right = 2;
       modules-left = "bspwm";
-      modules-right = "date";
-      padding-left = 0;
-      padding-right = 2;
-      radius = "6.0";
+      modules-right = "pulseaudio battery date";
+      
       tray-padding = 2;
       tray-position = "right";
-      width = "100%";
       wm-restack = "bspwm";
     };
+
     "module/bspwm" = {
       label-empty = "%index%";
       label-empty-foreground = "\${colors.foreground-alt}";
@@ -40,13 +46,42 @@
       label-urgent-padding=2;
       type="internal/bspwm";
     };
+
+    "module/battery" = {
+      type = "internal/battery";
+      battery = "BAT1";
+      adapter = "ACAD";
+      full-at = 98;
+      format-charging = "<ramp-capacity> <label-charging>";
+          
+      format-discharging = "<ramp-capacity> <label-discharging>";
+      format-discharging-underline = "#FF0000";
+          
+      format-full-prefix = " ";
+      format-full-prefix-foreground = "\${colors.foreground}";
+          
+      ramp-capacity-0 = " ";
+      ramp-capacity-1 = " ";
+      ramp-capacity-2 = " ";
+    };
+
     "module/date" = {
-      date="%d.%m.%y";
+      format-prefix = " ";
+      date-alt=" %d-%m-%Y";
       internal=5;
       label="%time%  %date%";
-      time="%H:%M";
+      time="%I:%M";
+      time-alt="%I:%M:%S";
       type="internal/date";
     };
+
+    "module/pulseaudio" = {
+      type = "internal/pulseaudio";
+      # sink = "alsa_output.pci-0000_12_00.3.analog-stereo";
+      use-ui-max = "true";
+      interval = 5;
+    };
+
     "colors" = {
       alert = "#bd2c40";
       background = "#222";
