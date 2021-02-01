9d9660315fac97a4256144a887fb6715e6e37803
Joel <cjjoelx42@gmail.com>
2021-02-01 16:10:46 +0530

    Add sound controls

--- configs/sxhkd.nix
+++ configs/sxhkd.nix
@@ -8,5 +8,9 @@
     "super + {_,shift + }w" = "${pkgs.bspwm}/bin/bspc node -{c,k}";
     "super + {_,shift + }{1-9,0}" = "${pkgs.bspwm}/bin/bspc {desktop -f,node -d} '^{1-9,10}'";
     "super + alt + {q,r}" = "${pkgs.bspwm}/bin/bspc {quit,wm -r}";
+    
+    "XF86AudioRaiseVolume" = "${pkgs.pulseaudio}/bin/pactl set-sink-volume 0 +5%";
+    "XF86AudioLowerVolume" = "${pkgs.pulseaudio}/bin/pactl set-sink-volume 0 -5%";
+    "XF86AudioMute" = "${pkgs.pulseaudio}/bin/pactl set-sink-mute 0 toggle";
   };
 }
