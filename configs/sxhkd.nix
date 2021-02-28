57d72435bb3ffcb8d08eae61bb80377ee70cde6f
Joel <cjjoelx42@gmail.com>
2021-02-28 18:12:09 +0530

    Add screenlock shortcut

--- configs/sxhkd.nix
+++ configs/sxhkd.nix
@@ -8,6 +8,7 @@
     "super + {_,shift + }w" = "${pkgs.bspwm}/bin/bspc node -{c,k}";
     "super + {_,shift + }{1-9,0}" = "${pkgs.bspwm}/bin/bspc {desktop -f,node -d} '^{1-9,10}'";
     "super + alt + {q,r}" = "${pkgs.bspwm}/bin/bspc {quit,wm -r}";
+    "super + l" = "XSECURELOCK_SHOW_HOSTNAME=0 XSECURELOCK_SHOW_USERNAME=0 ${pkgs.xsecurelock}/bin/xsecurelock";
     
     "XF86AudioRaiseVolume" = "${pkgs.pulseaudio}/bin/pactl set-sink-volume 0 +5%";
     "XF86AudioLowerVolume" = "${pkgs.pulseaudio}/bin/pactl set-sink-volume 0 -5%";
