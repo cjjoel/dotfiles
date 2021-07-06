24c5257aa4b31ac4035a4638b9ff31a82395fb89
Joel <cjjoelx42@gmail.com>
2021-07-06 22:06:41 +0530

    Add bspwm config

--- desktop/bspwm.nix
+++ desktop/bspwm.nix
@@ -1,11 +1,19 @@
 { pkgs, ... }:
 
 {
+  xsession.enable = true;
+  xsession.windowManager.bspwm = {
+    enable = true;
+    monitors =  { eDP1 = [ "I" "II" "III" "IV" "V" ]; };
+    startupPrograms = [ "sxhkd" "feh --bg-fill ~/.wallpaper" ];
+  };
+
   home.packages = with pkgs; [
     betterlockscreen
     sxhkd
+    feh
   ]; 
-  xdg.configFile."bspwm/bspwmrc".source = ../bin/bspwmrc;
+
   xdg.configFile."sxhkd/sxhkdrc".source = ../bin/sxhkdrc;
   services.picom.enable = true;
   services.polybar = import ../configs/polybar.nix { inherit pkgs; };
